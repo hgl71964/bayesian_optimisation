c1a52fcbef8f82c74e4855962f42db8f43f53c30
Mac station <guoliang.he@s-cube.com>
2020-12-22 11:54:31 +0000

    lazy_commie

--- global_search_BO/src/es.py
+++ global_search_BO/src/es.py
@@ -1,7 +1,5 @@
-import numpy as np
 import torch as tr
 import copy
-from time import sleep
 
 
 class evolutional_strategy:
@@ -16,3 +14,30 @@ class evolutional_strategy:
 
         
         return 
+
+
+class random_opt:
+
+    def __init__(self):
+        self.device = tr.device("cpu")
+
+    def outer_loop(
+                self,
+                T: int,  # iteration to run
+                x0: tr.Tensor,  # initial position; shape (2,)
+                r0: float,  # unormalised reward
+                api: callable,  # return functional evaluation
+                batch_size: int,  # random search is highly parallisable
+                ):
+        input_dim = x.shape[-1]
+        x, y = tr.zeros((1+T * batch_size, input_dim)), tr.zeros((1+T, ))
+        x[0], y[0] = x0.flatten(), api(x0, r0, self.device).flatten() 
+
+        for i in range(T):
+            query = tr.rand((batch_size, input_dim))
+            reward = api(query, r0, self.device).flatten()  # bottleneck for random search;
+            
+            x[1+i * batch_size : 1 + (i+1) * batch_size], \
+                y[1+i * batch_size : 1 + (i+1) * batch_size] = query, reward
+                
+        return x, y
