2d3a5b37d955e2d05a1b92f1f8466f36d392fc87
Mac station <guoliang.he@s-cube.com>
2020-12-24 13:19:33 +0000

    lazy_commie

---
+++ call.py
@@ -0,0 +1,9 @@
+import os
+from global_search_BO.interface_global import bayes_opt 
+
+
+print(os.getcwd())
+
+print(bayes_opt.device)
+
+
---
+++ global_search_BO/interface_global/bayes_opt.py
@@ -0,0 +1,11 @@
+"""import as a package"""
+import os
+
+from ..src.bayes_opt import bayesian_optimiser
+from ..src.api_helper import api_utils
+from ..src.api_helper import env
+
+
+
+
+
